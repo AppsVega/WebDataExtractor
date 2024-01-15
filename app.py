6ab1d4b4519a51096be1275c8886623353601e61
MateusVega <mateusggvega@gmail.com>
2024-01-15 09:33:53 -0300

    try and except atualization

--- app.py
+++ app.py
@@ -6,14 +6,17 @@ site = input("URL do site: ")
 txpath = input("XPATH do Titulo: ")
 pxpath = input("XPATH do Preço: ")
 print("="*20)
-driver = webdriver.Chrome()
-driver.get(site)
-Titles = driver.find_elements(By.XPATH,txpath)
-Prices = driver.find_elements(By.XPATH,pxpath)
-workbook = openpyxl.Workbook()
-Sheet_Produtos = workbook["Sheet"]
-Sheet_Produtos['A1'].value = "Nomes"
-Sheet_Produtos['B1'].value = "Preços"
-for Title,Price in zip(Titles,Prices):
-    Sheet_Produtos.append([Title.text,Price.text])
-workbook.save("Produtos.xlsx")
+try:
+    driver = webdriver.Chrome()
+    driver.get(site)
+    Titles = driver.find_elements(By.XPATH,txpath)
+    Prices = driver.find_elements(By.XPATH,pxpath)
+    workbook = openpyxl.Workbook()
+    Sheet_Produtos = workbook["Sheet"]
+    Sheet_Produtos['A1'].value = "Nomes"
+    Sheet_Produtos['B1'].value = "Preços"
+    for Title,Price in zip(Titles,Prices):
+        Sheet_Produtos.append([Title.text,Price.text])
+    workbook.save("Produtos.xlsx")
+except:
+    print("Não foi possivel entrar no site ou criar o arquivo xlsx")
